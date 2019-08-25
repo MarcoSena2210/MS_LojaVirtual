f366eadb36a628c505743d30060144f2c353b81b
Marco Sena <msena2210@gmail.com>
2019-08-24 22:13:51 -0300

    s08.47-Adicionando _layout-HomeControle-Page de Contato
    Adicionando layout:
    Controller Home
    Page Index
    Page Contato

---
+++ MS_LojaVirtual/MS_LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MS_LojaVirtual.Models;
+
+namespace MS_LojaVirtual.Controllers
+{
+     public class ProdutoController : Controller
+      {
+          public ActionResult Visualizar() 
+          {
+               Produto produto = GetProduto();
+               
+               
+               return View(produto);
+        //       return  new ContentResult() { Content = "<h3>Produto ==> visualizar </h3>", ContentType = "text/html" };
+          }
+
+          private Produto GetProduto()
+          {
+               return new Produto()
+               {
+                    Id = 1,
+                    Nome = "Bruna",
+                    Descricao = "Pulseira de primeira ",
+                    Valor = 10.00M
+               };
+            
+          }
+     }
+}
---
+++ MS_LojaVirtual/MS_LojaVirtual/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace MS_LojaVirtual.Models
+{
+     public class Produto
+     {
+          public int Id { get;  set; }
+          public string Nome { get;  set; }
+          public string Descricao { get;  set; }
+          public decimal Valor { get;  set; }
+     }
+}
