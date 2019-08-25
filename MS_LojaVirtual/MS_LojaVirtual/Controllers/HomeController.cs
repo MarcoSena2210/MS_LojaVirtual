d0e4e043a161d4ad87050bc89648d1cb2a6b0575
Marco Sena <msena2210@gmail.com>
2019-08-25 09:15:10 -0300

    HttpContext.Request Uma das formas de receber as informações do email.

--- MS_LojaVirtual/MS_LojaVirtual/Controllers/HomeController.cs
+++ MS_LojaVirtual/MS_LojaVirtual/Controllers/HomeController.cs
@@ -15,9 +15,13 @@ namespace MS_LojaVirtual.Controllers
           }
 
           public IActionResult ContatoAcao() {
-               return new ContentResult() { 
-                    Content = "Dados recebidos com sucesso!"
-               };
+
+               string nome = HttpContext.Request.Form["nome"];
+
+               string email = HttpContext.Request.Form["email"];
+               string texto = HttpContext.Request.Form["texto"];
+           
+               return new ContentResult() {        Content = string.Format( "Dados recebidos com sucesso! Nome: {0} <br/> E-mail: {1} <br/> Texto:{2} " , nome, email, texto), ContentType = "text/html"  };
           }
 
           public IActionResult Login()
