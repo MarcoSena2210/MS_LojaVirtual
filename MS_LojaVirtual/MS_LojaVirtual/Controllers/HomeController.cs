77db76f7e8dcfe8a4d005d87ceb5a3091e7e0a25
Marco Sena <msena2210@gmail.com>
2019-08-26 20:25:58 -0300

    s08.56.57-traduzindo as msgs e incluindo-as no model contato.

--- MS_LojaVirtual/MS_LojaVirtual/Controllers/HomeController.cs
+++ MS_LojaVirtual/MS_LojaVirtual/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using MS_LojaVirtual.Libraries.Email;
 using MS_LojaVirtual.Models;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace MS_LojaVirtual.Controllers
 {
@@ -18,24 +21,47 @@ namespace MS_LojaVirtual.Controllers
           }
 
           public IActionResult ContatoAcao() {
-
-               try {
+               try
+               {
                     Contato contato = new Contato();
                     contato.Nome = HttpContext.Request.Form["nome"];
                     contato.Email = HttpContext.Request.Form["email"];
                     contato.Texto = HttpContext.Request.Form["texto"];
 
-                    ContatoEmail.EnviarContatoPorEmail(contato);
+                    var listaMensagens = new List<ValidationResult>();
+                    var contexto = new ValidationContext(contato);
+                    bool isValid = Validator.TryValidateObject(contato, contexto, listaMensagens, true);
 
-                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
-               } catch (Exception) {
+                    if (isValid)
+                    {
+                         ContatoEmail.EnviarContatoPorEmail(contato);
 
-                    ViewData["MSG_E"] = "Atenção! Tivemos um erro, tente novamente mais tarde";
+                         ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                    }
+                    else
+                    {
+                         StringBuilder sb = new StringBuilder();
+                         foreach (var texto in listaMensagens)
+                         {
+                              sb.Append(texto.ErrorMessage + "<br />");
+                         }
 
-                    //TODO - Implementar log
+                         ViewData["MSG_E"] = sb.ToString();
+                    }
+
+
                }
+               catch (Exception )
+               {
+                    ViewData["MSG_E"] = "Opps! Tivemos um erro, tente novamente mais tarde!";
+
+                    //TODO - Implementar Log
+               }
+
+
                return View("Contato");
           }
+
 
           public IActionResult Login()
           {
--- MS_LojaVirtual/MS_LojaVirtual/Models/Contato.cs
+++ MS_LojaVirtual/MS_LojaVirtual/Models/Contato.cs
@@ -1,9 +1,21 @@
-﻿namespace MS_LojaVirtual.Models
+﻿using System.ComponentModel.DataAnnotations;
+using MS_LojaVirtual.Libraries.Lang;
+
+namespace MS_LojaVirtual.Models
 {
      public class Contato
      {
+          [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+          [MinLength(4, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")]
           public string Nome { get; set; }
+
+          [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+          [EmailAddress( ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E004")]
           public string Email { get; set; }
+
+          [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+          [MinLength(10, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")]
+          [MaxLength(1000,ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E003")]
           public string Texto { get; set; }
      }
 }
