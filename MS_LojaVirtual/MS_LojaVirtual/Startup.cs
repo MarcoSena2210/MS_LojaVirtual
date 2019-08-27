cb86acb7cc47d71c2824b780d3cbd97e63154f79
Marco Sena <msena2210@gmail.com>
2019-08-26 22:03:17 -0300

    s09.59-60-61-62-Configuração de banco de dados e 1ª Migration e Update Database

--- MS_LojaVirtual/MS_LojaVirtual/Startup.cs
+++ MS_LojaVirtual/MS_LojaVirtual/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MS_LojaVirtual.Database;
 
 namespace MS_LojaVirtual
 {
@@ -33,6 +35,10 @@ namespace MS_LojaVirtual
 
 
                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+               //inclusão do serviço de banco de dados 
+               string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LojaVirtual;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+               services.AddDbContext<LojaVirtualContext>(options => options.UseSqlServer(connection));
           }
 
           // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
